7fff20d5dae5d0c1bf6117a8ed3d050d1f6d9f74
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2023-05-05 19:19:17 +0530

    Add boilder code with required screens, navigations, and assets

---
+++ navigation/TabNavigator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TabNavigator = () => {
+  return (
+    <div>TabNavigator</div>
+  )
+}
+
+export default TabNavigator
---
+++ screens/ChangePlan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChangePlan = () => {
+  return (
+    <div>ChangePlan</div>
+  )
+}
+
+export default ChangePlan
