ac7abf96cb7e5c4703402bfbd90d4a8127190506
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2023-05-06 15:18:27 +0530

    Add navigation

--- navigation/StackNavigator.jsx
+++ navigation/StackNavigator.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Splash from '../screens/Splash'
+import SignUp from '../screens/SignUp'
+import Login from '../screens/Login'
+import TabNavigator from './TabNavigator'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const screenOptionStyle = {
+  headerStyle: {
+    backgroundColor: "#0287e3",
+  },
+  headerTintColor: "white",
+  headerBackTitle: "Back",
+};
 
 const StackNavigator = () => {
   return (
-    <div>StackNavigator</div>
+    <Stack.Navigator initialRouteName="MyFone" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="MyFone" component={Splash}  />
+      <Stack.Screen name="Auth" component={AuthStackNavigator} />
+      <Stack.Screen name="Tabs" component={TabNavigator} />
+    </Stack.Navigator>
+  )
+}
+
+const AuthStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Sign Up" component={SignUp} />
+      <Stack.Screen name="Log in" component={Login} />
+    </Stack.Navigator>
   )
 }
 
--- navigation/TabNavigator.jsx
+++ navigation/TabNavigator.jsx
@@ -1,8 +1,43 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
+import Splash from '../screens/Splash'
+import Home from '../screens/Home'
+import ChangePlan from '../screens/ChangePlan'
+import ContactUs from '../screens/ContactUs'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
+import { faFile } from '@fortawesome/free-solid-svg-icons/faFile'
+import { faUsers } from '@fortawesome/free-solid-svg-icons/faUsers'
 
-const TabNavigator = () => {
+const Tab = createBottomTabNavigator();
+
+const screenOptionStyle = {
+  headerStyle: {
+    backgroundColor: "#0287e3",
+  },
+  headerTintColor: "white",
+  headerBackTitle: "Back"
+};
+
+const TabNavigator = ({navigation}) => {
   return (
-    <div>TabNavigator</div>
+    <Tab.Navigator initialRouteName="My Account" screenOptions={screenOptionStyle}>
+      <Tab.Screen name="My Account" component={Home} options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesomeIcon icon={faHome} size={20} color="grey" />
+        ),
+      }}/>
+      <Tab.Screen name="Change Plan" component={ChangePlan} options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesomeIcon icon={faFile} size={20} color="grey" />
+        ),
+      }}/>
+      <Tab.Screen name="Contact Us" component={ContactUs} options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesomeIcon icon={faUsers} size={20} color="grey" />
+        ),
+      }}/>
+    </Tab.Navigator>
   )
 }
 
