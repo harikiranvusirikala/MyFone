4c28b074edc0768582a21a3ac0191262fd41ec29
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2023-05-06 14:12:42 +0530

    Load sample data in index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { plans } from './data/plans.json'
+import { issues } from './data/issues.json'
+
+const loadData = () => {
+	AsyncStorage.setItem(
+		'plans',
+		JSON.stringify(plans),
+	);
+	AsyncStorage.setItem(
+		'issues',
+		JSON.stringify(issues),
+	);
+}
+
+// Loading the dummy data to AsyncStorage
+loadData();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
